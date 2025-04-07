5262f63db00ad912f6d027b3488cd548d08a6b07
Katy Tyi <d.boy.dragoz@gmail.com>
2025-04-07 13:50:14 +0200

    Update 2.1

--- js/main.js
+++ js/main.js
@@ -10,3 +10,15 @@ function toggleBurger() {
 	let burger = document.getElementById("burger-menu")
 	burger.classList.toggle("burger-show")
 }
+
+fetch("/import/header.html")
+.then(res => res.text())
+.then(data => {
+	document.querySelector("#header").innerHTML = data;
+});
+
+fetch("/import/footer.html")
+.then(res => res.text())
+.then(data => {
+	document.querySelector("#footer").innerHTML = data;
+});
